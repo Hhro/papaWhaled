04fe9ea9b7bad07f17ea586e06768a82cc54c156
Hhro <kwonsw212@gmail.com>
2020-01-15 01:12:38 +0900

    ssl now possible

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-import os
+import sys
+import argparse
 from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
+from flask_sslify import SSLify
 from resources.challs import ChallengeListAPI
 from resources.challs import ChallengeUploadAPI
 from resources.challs import ChallengeRestartAPI
@@ -16,5 +18,18 @@ api.add_resource(ChallengeUploadAPI,'/challs/upload',endpoint="challenge_upload"
 api.add_resource(ChallengeRestartAPI,'/challs/restart',endpoint="challenge_restart")
 api.add_resource(ChallengeTerminateAPI,'/challs/term',endpoint="challenge_terminate")
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port="31337")
+parser = argparse.ArgumentParser()
+parser.add_argument("--ssl",action="store_true")
+parser.add_argument("--pem")
+parser.add_argument("--crt")
+parser.add_argument("-d","--debug",action="store_true")
+parser.add_argument("-p","--port")
+args = parser.parse_args()
+
+if args.ssl:
+    context = (args.crt,args.pem)
+    sslify = SSLify(app)
+
+    app.run(debug=args.debug, host='0.0.0.0', port=args.port, ssl_context=context)
+else:
+    app.run(debug=args.debug, host='0.0.0.0', port=args.port)
