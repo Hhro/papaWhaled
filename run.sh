30d8856e40ba4d0e6afdc0777162b4ece8b8e89b
Hhro <kwonsw212@gmail.com>
2020-01-23 18:35:15 +0900

    .

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source ./papaWhaled_env/bin/activate
-nohup python app.py -d -p 31337 --supplier /home/hhro/papaWhaled/supplier/ --pem ssl/hhro.key --crt ssl/hhro.crt --ssl &
+nohup python app.py -d -p 31337 --supplier ./supplier --pem ssl/hhro.key --crt ssl/hhro.crt --ssl &
